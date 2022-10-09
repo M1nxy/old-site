7c5967a5ca678a6dac8f7de1fdcd776267299139
Alucard <minxter.yt@gmail.com>
2022-10-09 23:30:07 +0100

    Bug Fixes (Light mode)
    Fixed light mode rendering
    Fixed typo
    Added shadow for greater legibility

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,15 @@
   <div class="bg-base-300 h-full w-full flex flex-col">
     <NavBar></NavBar>
     <div class="content flex justify-center items-center h-full overflow-auto">
-      <div class="mockup-window border bg-base-300 h-fit mx-4 md:mx-0">
+      <div class="mockup-window border border-current bg-base-400 h-fit mx-4 md:mx-0 shadow-2xl">
         <div class="hero bg-base-200 h-full">
           <div class="hero-content flex-col lg:flex-row">
             <img src="../static/m1nx.jpg" class="max-w-xs rounded-lg p-5 rounded-full" />
             <div>
               <h1 class="text-5xl font-bold">M1nx</h1>
               <p class="py-6">I am a fullstack developer with experience using multiple frontend and backend web frameworks.</p>
-              <a class="btn" href="./knowledge">Knowledge</a>
-              <a class="btn" href="./projects">Projects</a>
+              <a class="btn btn-ghost bg-base-300 text-current" href="./knowledge">Knowledge</a>
+              <a class="btn btn-ghost bg-base-300 text-current" href="./projects">Projects</a>
             </div>
           </div>
         </div>
--- pages/projects.vue
+++ pages/projects.vue
@@ -3,39 +3,39 @@
     <NavBar></NavBar>
     <div class="content flex justify-center items-center h-full overflow-auto">
       <div class="container justify-between m-auto px-8 md:px-14 lg:px-24 w-full">
-        <h2 class="text-5xl font-bold pb-10 py-2">My Projects</h2>
-        <div class="flex gap-6 flex-wrap">
+        <h2 class="text-5xl font-bold py-6">My Projects</h2>
+        <div class="flex gap-6 flex-wrap pb-4">
           <div class="w-full basis-1/1 grow lg:basis-1/3 shadow-2xl bg-base-100 rounded-lg p-10 flex flex-col justify-between">
             <h2 class="font-sm font-bold text-highlight">Oct 2022</h2>
-            <h2 id="work" class="text-3xl font-bold text-white">Gib</h2>
+            <h2 id="work" class="text-3xl font-bold text-current">Gib</h2>
             <p class="section-paragraph">A nodejs project manager with license generation, dependency management and other QOL tools.</p>
             <br>
             <a href="https://github.com/M1nxy/gib" target="_blank" class="text-gray-400 hover:text-selected-text after:content-['_↗']">Visit the project</a>
           </div>
           <div class="w-full basis-1/1 grow lg:basis-1/3 shadow-2xl bg-base-100 rounded-lg p-10 flex flex-col justify-between">
             <h2 class="font-sm font-bold text-highlight">Oct 2022</h2>
-            <h2 id="work" class="text-3xl font-bold text-white">Helios</h2>
+            <h2 id="work" class="text-3xl font-bold text-current">Helios</h2>
             <p class="section-paragraph">A successor to ScamBeGone with extended multimedia functionality and other features.</p>
             <br>
             <a href="https://github.com/M1nxy/helios" target="_blank" class="text-gray-400 hover:text-selected-text after:content-['_↗']">Visit the project</a>
           </div>
           <div class="w-full basis-1/1 grow lg:basis-1/3 shadow-2xl bg-base-100 rounded-lg p-10 flex flex-col justify-between">
             <h2 class="font-sm font-bold text-highlight">Jul 2022</h2>
-            <h2 id="work" class="text-3xl font-bold text-white">Repository Aggregator</h2>
+            <h2 id="work" class="text-3xl font-bold text-current">Repository Aggregator</h2>
             <p class="section-paragraph">A small tool for aggregating data from multiple iOS Tweak repository databases</p>
             <br>
             <a href="https://github.com/M1nxy/repository-aggregator" target="_blank" class="text-gray-400 hover:text-selected-text after:content-['_↗']">Visit the project</a>
           </div>
           <div class="w-full basis-1/1 grow lg:basis-1/3 shadow-2xl bg-base-100 rounded-lg p-10 flex flex-col justify-between">
             <h2 class="font-sm font-bold text-highlight">Feb 2022</h2>
-            <h2 id="work" class="text-3xl font-bold text-white">ScamBeGone</h2>
+            <h2 id="work" class="text-3xl font-bold text-current">ScamBeGone</h2>
             <p class="section-paragraph">A multipurpose discord bot with scam avoidance and other moderation tools.</p>
             <br>
             <a href="https://github.com/M1nxy/scambegone" target="_blank" class="text-gray-400 hover:text-selected-text after:content-['_↗']">Visit the project</a>
           </div>
           <div class="w-full basis-1/1 grow lg:basis-1/3 shadow-2xl bg-base-100 rounded-lg p-10 flex flex-col justify-between">
             <h2 class="font-sm font-bold text-highlight">Feb 2022</h2>
-            <h2 id="work" class="text-3xl font-bold text-white">String Flagger</h2>
+            <h2 id="work" class="text-3xl font-bold text-current">String Flagger</h2>
             <p class="section-paragraph">An npm package to check if a string contains any string or domain from a filter array.</p>
             <br>
             <a href="https://github.com/M1nxy/string-flagger" target="_blank" class="text-gray-400 hover:text-selected-text after:content-['_↗']">Visit the project</a>
