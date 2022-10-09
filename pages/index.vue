7f507e49ab6bc1d9a17bcb1362e9f34a1d963866
Alucard <minxter.yt@gmail.com>
2022-10-09 21:47:21 +0100

    First commit
    Basic site layout etc
    Thx @exerra for language card layout etc

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-base-300 h-full w-full flex flex-col">
+    <NavBar></NavBar>
+    <div class="content flex justify-center items-center h-full overflow-auto">
+      <div class="mockup-window border bg-base-300 h-fit mx-4 md:mx-0">
+        <div class="hero bg-base-200 h-full">
+          <div class="hero-content flex-col lg:flex-row">
+            <img src="../static/m1nx.jpg" class="max-w-xs rounded-lg p-5 rounded-full" />
+            <div>
+              <h1 class="text-5xl font-bold">M1nx</h1>
+              <p class="py-6">I am a fullstack developer with experience using multiple frontend and backend web frameworks.</p>
+              <a class="btn" href="./knowledge">Knowledge</a>
+              <a class="btn" href="./projects">Projects</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer class="w-full text-center text-l">© M1nx 2022</Footer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+  #__layout {
+    height: 100vh;
+  }
+</style>
